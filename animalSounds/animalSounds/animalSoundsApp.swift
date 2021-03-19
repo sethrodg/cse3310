62716e4bc0b321ae17431ad150dec54fc31ea30a
Seth Rodgers <seth.rodgers@mavs.uta.edu>
2021-03-19 18:48:42 -0500

    add xcode project files

---
+++ animalSounds/animalSounds/animalSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animalSoundsApp.swift
+//  animalSounds
+//
+//  Created by Seth Rodgers on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct animalSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
