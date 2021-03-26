d9010e667cf3bc2b1243ace0299376a6c96b20cb
Seth Rodgers <seth.rodgers@mavs.uta.edu>
2021-03-26 18:00:46 -0500

    created page navigation

--- animalSounds/animalSounds/ContentView.swift
+++ animalSounds/animalSounds/ContentView.swift
@@ -10,21 +10,67 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+        
+        
         NavigationView{
-            NavigationLink(
-                destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/,
-                label: {
-                    /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/
-                })
-            Text("Hello, world!")
-                .padding().navigationBarTitle("Navigation", displayMode: .automatic)
+            //Text("Login")
+//                .padding().navigationBarTitle("Animal Sounds", displayMode: .automatic)
+//                .font(.title)
+//                .foregroundColor(.blue)
+            
+            pageOne().navigationBarTitle("Page One", displayMode: .inline)
         }
+        
+        
         
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+
+struct pageOne: View{
+    var body: some View{
+        
+        VStack{
+            Text("Page one").navigationBarTitle("Page one")
+            NavigationLink(destination: pageTwo()){
+                Text("Go to page two")
+            }
+        }
+    }
+}
+
+
+struct pageTwo: View{
+    var body: some View{
+        
+        VStack{
+            Text("Page two").navigationBarTitle("Page two")
+            NavigationLink(destination: pageThree()){
+                Text("Go to page three")
+            }
+        }
+    }
+}
+
+
+struct pageThree: View{
+    var body: some View{
+        
+        VStack{
+            Text("Page three").navigationBarTitle("Page three")
+//            NavigationLink(destination: pageThree()){
+//                Text("Go to page two")
+//            }
+        }
     }
 }
