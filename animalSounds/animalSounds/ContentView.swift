27227bb79b5781f52e1e2a3cb40aff0ece534f69
Seth Rodgers <seth.rodgers@mavs.uta.edu>
2021-03-28 23:34:49 -0500

    updated login page

--- animalSounds/animalSounds/ContentView.swift
+++ animalSounds/animalSounds/ContentView.swift
@@ -6,6 +6,27 @@
 //
 
 import SwiftUI
+import CoreGraphics
+
+
+//struct Profile {
+//    var username: String
+//    var password: String
+//    var joinDate = Date()
+//
+//    static let `default` = Profile(username: "user123", password: "123")
+//}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -16,13 +37,11 @@ struct ContentView: View {
     
         NavigationView{
             
-            pageOne().navigationBarTitle("Page One", displayMode: .inline)
+            pageOne().navigationBarTitle("Animal Sounds", displayMode: .inline)
         }
         
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -31,38 +50,119 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
+//PAGE ONE
 struct pageOne: View{
+    
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    var isLoggedin:Bool = false
+
+    
+    
     var body: some View{
         
+        
+        
         VStack{
-            Text("Page one").navigationBarTitle("Page one")
+            
+            Text("").navigationBarTitle("Animal Sounds")
+            
+            //USERNAME
+            TextField("Username", text: $username)
+                .padding()
+                .background(Color(red: 230 / 255, green: 230 / 255, blue: 230 / 255))
+                .cornerRadius(8.0)
+                .padding()
+            
+            //PASSWORD
+            SecureField("Password", text: $password)
+                .padding()
+                .background(Color(red: 230 / 255, green: 230 / 255, blue: 230 / 255))
+                .cornerRadius(8.0)
+                .padding()
+            
             NavigationLink(destination: pageTwo()){
-                Text("Go to page two")
+                Text("Login")
             }
-
         }
     }
 }
 
 
+
+//PAGE TWO - HOME PAGE
 struct pageTwo: View{
     var body: some View{
         
         VStack{
-            Text("Page two").navigationBarTitle("Page two")
+            Text("").navigationBarTitle("Home page")
+            
             NavigationLink(destination: pageThree()){
-                Text("Go to page three")
+                Text("Classification Page")
             }
+            NavigationLink(destination: pageFour()){
+                Text("Profile Page")
+            }
+            NavigationLink(destination: pageFive()){
+                Text("Model Info")
+            }
+            NavigationLink(destination: pageSix()){
+                Text("Statistics")
+            }
+            
+            
         }
     }
 }
 
 
+
+//PAGE THREE - CLASSIFICATION PAGE
 struct pageThree: View{
     var body: some View{
         
         VStack{
-            Text("Page three").navigationBarTitle("Page three")
+            Text("Classification Page").navigationBarTitle("Classification Page")
+        }
+    }
+}
+
+
+
+//PAGE FOUR - PROFILE PAGE
+struct pageFour: View{
+    var body: some View{
+        
+        VStack{
+            Text("Profile Page").navigationBarTitle("Profile Page")
+            //Text("Username: \(draftProfile.username)")
+
+        }
+    }
+}
+
+
+
+//PAGE FIVE - MODEL INFO
+struct pageFive: View{
+    var body: some View{
+        
+        VStack{
+            Text("Page Five").navigationBarTitle("Page five")
+
+        }
+    }
+}
+
+
+
+//PAGE SIX - STATISTICS
+struct pageSix: View{
+    var body: some View{
+        
+        VStack{
+            Text("").navigationBarTitle("Statistics")
 
         }
     }
